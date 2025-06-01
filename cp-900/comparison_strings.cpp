050d2b96ecbaa5dce88d14b5fe16c6672b94ae3f
AJAY GOUR <ajaygour3333@gmail.com>
2025-06-01 12:46:07 +0530

    added comparison strings

--- cp-900/comparison_strings.cpp
+++ cp-900/comparison_strings.cpp
@@ -1,27 +1,31 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-   int t;
-   cin>>t;
-   while(t--){
-      int n;
-      cin>>n;
-      string s;
-      cin>>s;
 
-      unordered_set<int>st;
-      int curr = 1;
-      st.insert(1);
-      for(int i = 0;i<n;i++){
-         if(s[i] == '<'){
-            st.insert(curr+1);
-            curr++;
-         }else{
-            st.insert(curr-1);
-            curr--;
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        string s;
+        cin >> s;
+
+        int ans = 1;
+        int curr = 1;
+
+        for(int i =1;i<n;i++){
+         if(s[i] == s[i-1]){
+            curr ++;
+            ans = max(ans,curr);
          }
-      }
-      cout<<st.size()<<endl;
-   }
+         else{
+            curr = 1;
+         }
+        }
+        ans = max(ans,curr);
+        cout<<ans+1<<endl;
+
+        
+    }
 }
