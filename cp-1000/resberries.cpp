b590fb1e1efb6d19f70d3853c7bfd73d22a8fbb2
ajay gour <ajaygour@ajays-MacBook-Air.local>
2025-07-01 22:09:28 +0530

    1 question

--- cp-1000/resberries.cpp
+++ cp-1000/resberries.cpp
@@ -11,36 +11,80 @@ int main() {
     while(t--){
         int n,k;
         cin>>n>>k;
+        vector<int>v(n);
+        int cnt1 = 0;
+        for(int i = 0;i<n;i++){
+            cin>>v[i];
+            if (v[i] == 1)
+                cnt1++;
+        }
 
         if(k == 2){
-            if(n%2 == 0)cout<<0;
-            else cout<<1;
+            bool flag = false;
+            for(int ele:v){
+                if(ele%2 == 0){
+                    cout<<0;
+                    flag = true;
+                    break;
+                }
+            }
+            if(!flag){
+                cout<<1;
+                
+            }
         }
         else if(k == 3){
-            if(n%3 == 0)cout<<0;
-            else if(n%3 == 1)cout<<2;
-            else cout<<1;
+            bool flag = false;
+            int res = 2;
+            for (int ele : v)
+            {
+                if (ele % 3 == 0)
+                {
+                    cout << 0;
+                    flag = true;
+                    break;
+                }
+                res = min(res, 3 - ele % 3);
+            }
+            if (!flag)
+                cout << res;
         }
         else if(k == 4){
-            if (n % 4 == 0)
-                cout << 0;
-            else if (n % 4 == 1)
-                cout << 3;
-            else if (n % 4 == 2)
-                cout << 2;
-            else cout<<3;
+            int res = 3;
+            bool flag = false;
+            for (int ele : v)
+            {
+                if (ele % 4 == 0)
+                {
+                    cout << 0;
+                    flag = true;
+                    break;
+                }
+                res = min(res, 4 - ele % 4);
+                
+            }
+            if (cnt1 >= 2)
+            {
+                res = min(res, 2);
+            }
+            if (!flag)
+                cout << res;
         }
         else{
-            if (n % 5 == 0)
-                cout << 0;
-            else if (n % 5 == 1)
-                cout << 4;
-            else if (n % 5 == 2)
-                cout << 3;
-            else if (n % 5 == 3)
-                cout << 2;
-            else
-                cout << 1;
+            int res = 4;
+            bool flag = false;
+            for(int ele:v){
+                if(ele % 5 == 0 ){
+                    cout<<0;
+                    flag = true;
+                    break;
+                    
+                }
+                res = min(res, 5 - ele % 5);
+            }
+            if(!flag)cout<<res;
+            
+
         }
         cout<<endl;
     }
